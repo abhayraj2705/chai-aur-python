5582911254eb681b3e4c9005ae721ad06e53e151
Abhayraj B <abhayraj27052005@gmail.com>
2025-10-20 00:57:58 +0530

    in this we learned about the inner working of the python

---
+++ 02_basics/02_basics.py
@@ -0,0 +1,26 @@
+# going to see the inner working of the python 
+# internals of the python 
+
+# python make the byte code in it is the pychache folder 
+# then the python the vm runs the code . byte code is fethed on the vms and then it is runned 
+# source code is compiled down 
+# byte code is the low level code platform independent 
+
+# byte code runs faster 
+# .pyc is the compiled python is also called the frozen binaries 
+
+# --pychahce-- are created by the python for the oraganising 
+# the above contains the source change and python version see the files changed 
+# cpython is the version nameing of the python ex 3.12 
+# .pyc files cames under the imported files 
+
+# pvm is the software of the vms and runs the code 
+# also known python interpretor 
+# run time engine -- can also called 
+
+# note the byte code is not the machine code 
+# because it is not the hardware 
+# it is the python specific interpretation 
+
+# cpython is default 
+# also there are the j python another versions of the python 
