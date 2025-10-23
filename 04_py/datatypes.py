88027902d05dc7eb9a8c7d73b219b11638d6d870
Abhayraj B <abhayraj27052005@gmail.com>
2025-10-24 01:07:15 +0530

    this is the basics for the datatypes

---
+++ 04_py/datatypes.py
@@ -0,0 +1,22 @@
+# this is the lecture for the python datatypes 
+
+# object types or datatypes
+
+#-NUmbers = 1234
+# float 
+# int 
+# boolean : True , False 
+# tuples : () in tuples we contains the parentisis , nametuple 
+# arrays 
+# lists : [] (square brackets) we get the continuous memory space for the stroing the values also contains the methods
+# string : 'spam',"bobs" this both are treated as the strings 
+# dictionary:{'food"} we conatains  the curly braces 
+# sets : contains only uniques elements
+# file : open ('eggs.txt)
+# Functions , modules , classes , 
+
+# special types are , Adavanced 
+
+# Decorators , Generators , Meta programming 
+
+# we can say that the list can be say as the arrays 
