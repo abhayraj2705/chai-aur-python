5e117aa5e905c027a5945f5eef9a5e3ff8cfc749
Abhayraj B <abhayraj27052005@gmail.com>
2025-10-24 01:43:44 +0530

    this is the indepth knowledge for the python internal

---
+++ 05_py/internal.py
@@ -0,0 +1,17 @@
+# the python contains  the ref count for the number 
+# every datatypes are in the memory 
+# inside the memory the dataype is there 
+
+# the python do the garbage collection of the numbers late 
+
+# see python behaviour of the python is based on the refrences 
+
+# if you give one refrence to the list then the list is the mutual with the list which is refrenced 
+
+# if the lists contains the more than one rferences then the particular list is immutuable 
+
+# h1=[1,2,3,] , h2[:] this is the example of the sclicing
+
+# see this syntax checks the memory refrences ex m is m 
+
+# 
